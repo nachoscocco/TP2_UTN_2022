d9c2b30796b3579dc889469a52ca504d87eb8e33
utnnet2019@outlook.com <utnnet2019@outlook.com>
2022-06-06 20:09:09 -0300

    unit 3 lab 04 -> se llego al punto 26

---
+++ LAbs/TP2_Lab04/TP2/Business.Entities/Domicilio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    class Domicilio
+    {
+        public int IdDomicilio { get; set; }
+        public string Calle { get; set; }
+        public int Numero { get; set; }
+        public string LetraBis { get; set; }
+        public int Piso { get; set; }
+        public char Depto { get; set; }
+        public string Pais_s { get; set; }
+        public string Provincia_s { get; set; }
+        public string Localidad_s { get; set; }
+        public string Telefono { get; set; }
+        public string Telefono2 { get; set; }
+    }
+}
---
+++ LAbs/TP2_Lab04/TP2/UI.Desktop/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class Usuario : Form
+    {
+        public Usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
