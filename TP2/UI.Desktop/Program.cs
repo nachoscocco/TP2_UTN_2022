bc8814c7e1c442acbe15348e5b6709637c3b4c7f
Gamba <emilianofranciscogamba@outlook.com>
2022-07-08 02:57:44 -0300

    Agregue formulario main sin errores

--- TP2/UI.Desktop/Program.cs
+++ TP2/UI.Desktop/Program.cs
@@ -17,7 +17,7 @@ namespace UI.Desktop
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ABMsForms.Conocimiento());
+            Application.Run(new Main());
         }
     }
 }
